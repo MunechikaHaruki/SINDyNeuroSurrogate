aa634820160d03dcd3373b465e8d881617124113
MunechikaHaruki <h.m.haruki.160@gmail.com>
2026-03-25 03:42:12 +0900

    refactor: 💡 log_dataset_cfgとlog_eval_resultを抜き出す

--- scripts/main.py
+++ scripts/main.py
@@ -13,18 +13,13 @@ from utils.builder import (
 )
 from utils.log_utils import (
     get_hydra_overrides,
+    log_dataset_cfg,
+    log_eval_result,
     log_surrogate_model,
     log_surrogate_summary,
-    save_xarray,
 )
 
 from neurosurrogate.modeling.calc_engine import unified_simulator
-from neurosurrogate.modeling.profiler import calc_dynamic_metrics
-from neurosurrogate.utils.plots import (
-    draw_engine,
-    plot_2d_attractor_comparison,
-    spec_diff,
-)
 
 logger = logging.getLogger(__name__)
 
@@ -38,56 +33,24 @@ def train_model(surrogate, train_ds, target_comp_id):
 
 @mlflow.trace
 def eval_diff(dataset_cfg, surrogate_model):
-    mlflow.log_params(dataset_cfg)
-    mlflow.log_params(dataset_cfg["current"]["pipeline"][0])
-    mlflow.log_dict(dataset_cfg, "dataset.yaml")
-
+    log_dataset_cfg(dataset_cfg)
     original_ds = unified_simulator(**build_simulator_config(dataset_cfg))
-
     target_comp_id = dataset_cfg["target_comp_id"]
-
     predict_result = unified_simulator(
         **build_simulator_config(dataset_cfg),
         surrogate_target=target_comp_id,
         surrogate_model=surrogate_model,
     )
-
     preprocessed_xr = surrogate_model.preprocessor.transform(
         original_ds, target_comp_id=target_comp_id
     )
-
-    # logging
-    dt = dataset_cfg["dt"]
-    mlflow.log_metrics(
-        calc_dynamic_metrics(original_ds, predict_result, target_comp_id, dt)
-    )
-    names = ["orig", "preprocessed", "surr"]
-    datasets = [original_ds, preprocessed_xr, predict_result]
-    for ds, name in zip(datasets, names):
-        save_xarray(ds, name)
-
-    datasets, spec = spec_diff(
-        original_ds, preprocessed_xr, predict_result, surr_id=target_comp_id
-    )
-    mlflow.log_figure(
-        draw_engine(datasets, spec, engine="matplotlib"),
-        artifact_file="compare.png",
-    )
-
-    fig_phase = plot_2d_attractor_comparison(
-        orig_ds=preprocessed_xr,
-        surr_ds=predict_result,
-        comp_id=target_comp_id,
-        state_vars=["V", "latent1"],  # 実際のSINDyのターゲット変数名に合わせて変更
-    )
-    mlflow.log_figure(fig_phase, artifact_file="attractor_surr.png")
+    log_eval_result(original_ds, predict_result, preprocessed_xr, dataset_cfg)
 
 
 def main_flow(datasets_cfg: Dict, surrogate_model, run_name):
     logger.info("Start Flow:start generate train data")
     with mlflow.start_run(run_name=run_name) as run:
         logger.info(f"run_id:{run.info.run_id}")
-        mlflow.log_dict(datasets_cfg, "datasets.yaml")
         train_ds = unified_simulator(**build_simulator_config(datasets_cfg["train"]))
         target_comp_id = datasets_cfg["train"]["target_comp_id"]
         logger.info("Start Training")
--- scripts/utils/log_utils.py
+++ scripts/utils/log_utils.py
@@ -7,7 +7,14 @@ import mlflow
 import numpy as np
 
 from neurosurrogate.modeling import SINDySurrogateWrapper
-from neurosurrogate.utils.plots import draw_engine, plot_sindy_coefficients, spec_simple
+from neurosurrogate.modeling.profiler import calc_dynamic_metrics
+from neurosurrogate.utils.plots import (
+    draw_engine,
+    plot_2d_attractor_comparison,
+    plot_sindy_coefficients,
+    spec_diff,
+    spec_simple,
+)
 
 
 def get_hydra_overrides():
@@ -79,6 +86,40 @@ def log_surrogate_model(surrogate: SINDySurrogateWrapper):
                 "target_module_path": inspect.getfile(surrogate.target_module),
             },
         )
+
+
+def log_dataset_cfg(dataset_cfg):
+    mlflow.log_params(dataset_cfg)
+    mlflow.log_params(dataset_cfg["current"]["pipeline"][0])
+    mlflow.log_dict(dataset_cfg, "dataset.yaml")
+
+
+def log_eval_result(original_ds, predict_result, preprocessed_xr, dataset_cfg):
+    dt = dataset_cfg["dt"]
+    target_comp_id = dataset_cfg["target_comp_id"]
+    mlflow.log_metrics(
+        calc_dynamic_metrics(original_ds, predict_result, target_comp_id, dt)
+    )
+    names = ["orig", "preprocessed", "surr"]
+    datasets = [original_ds, preprocessed_xr, predict_result]
+    for ds, name in zip(datasets, names):
+        save_xarray(ds, name)
+
+    datasets, spec = spec_diff(
+        original_ds, preprocessed_xr, predict_result, surr_id=target_comp_id
+    )
+    mlflow.log_figure(
+        draw_engine(datasets, spec, engine="matplotlib"),
+        artifact_file="compare.png",
+    )
+
+    fig_phase = plot_2d_attractor_comparison(
+        orig_ds=preprocessed_xr,
+        surr_ds=predict_result,
+        comp_id=target_comp_id,
+        state_vars=["V", "latent1"],  # 実際のSINDyのターゲット変数名に合わせて変更
+    )
+    mlflow.log_figure(fig_phase, artifact_file="attractor_surr.png")
 
 
 def save_xarray(ds, name):
